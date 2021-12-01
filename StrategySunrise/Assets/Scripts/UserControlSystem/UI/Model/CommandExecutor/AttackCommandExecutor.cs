3a1798295b2c5c5584943454740dc146706ea257
sunriseq26 <sunriseq26@gmail.com>
2021-12-01 22:57:04 +0300

    Урок 3. Победил реализацию правильного флоу передвижения

---
+++ StrategySunrise/Assets/Scripts/UserControlSystem/UI/Model/CommandExecutor/AttackCommandExecutor.cs
@@ -0,0 +1,13 @@
+﻿using Abstractions.Commands;
+using Abstractions.Commands.CommandsInterfaces;
+
+namespace UserControlSystem
+{
+    public class AttackCommandExecutor : CommandExecutorBase<IAttackCommand>
+    {
+        public override void ExecuteSpecificCommand(IAttackCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- StrategySunrise/Assets/Scripts/UserControlSystem/UI/Model/UIModelInstaller.cs
+++ StrategySunrise/Assets/Scripts/UserControlSystem/UI/Model/UIModelInstaller.cs
@@ -8,17 +8,19 @@ namespace UserControlSystem
     public class UIModelInstaller : MonoInstaller
     {
         [SerializeField] private AssetsContext _legacyContext;
+        [SerializeField] private Vector3Value _vector3Value;
 
         public override void InstallBindings()
         {
             Container.Bind<AssetsContext>().FromInstance(_legacyContext);
+            Container.Bind<Vector3Value>().FromInstance(_vector3Value);
 
             Container.Bind<CommandCreatorBase<IProduceUnitCommand>>()
                 .To<ProduceUnitCommandCreator>().AsTransient();
-            Container.Bind<CommandCreatorBase<IMoveCommand>>()
-                .To<MoveCommandCreator>().AsTransient();
             Container.Bind<CommandCreatorBase<IAttackCommand>>()
                 .To<AttackCommandCreator>().AsTransient();
+            Container.Bind<CommandCreatorBase<IMoveCommand>>()
+                .To<MoveCommandCreator>().AsTransient();
             Container.Bind<CommandCreatorBase<IPatrolCommand>>()
                 .To<PatrolCommandCreator>().AsTransient();
             Container.Bind<CommandCreatorBase<IStopCommand>>()
